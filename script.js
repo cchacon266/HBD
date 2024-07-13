05780c5e6cae94dddf7fdd29895753f36b874062
Carlos Chacon Pren <cchacon266@gmail.com>
2024-07-13 04:54:44 -0600

    data add

--- script.js
+++ script.js
@@ -9,33 +9,6 @@ function showNextImage() {
 
 setInterval(showNextImage, 3000);
 
-function showModal(message, showImage) {
-    document.getElementById('modalText').innerHTML = message;
-    const modalContent = document.querySelector('.modal-content');
-    const sorryImage = document.getElementById('sorryImage');
-    if (showImage) {
-        sorryImage.style.display = "block";
-    } else {
-        sorryImage.style.display = "none";
-    }
-    modalContent.classList.add('large'); // Agregar la clase 'large' al modal
-    document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-    const modalContent = document.querySelector('.modal-content');
-    modalContent.classList.remove('large'); // Remover la clase 'large' al cerrar el modal
-    document.getElementById('myModal').style.display = "none";
-}
-
-
-function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-}
-
-window.onclick = function(event) {
-    const modal = document.getElementById('myModal');
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function playMusic() {
+    document.getElementById('backgroundMusic').play();
 }
